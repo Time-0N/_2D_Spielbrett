aea73de605a9c6fd680d297abe84dd26ce0cf584
Time_ON <haas.timeon@gmail.com>
2023-11-06 16:50:39 +0100

    Alpha 0.0.1v

--- src/main/java/_2D_PlayerBoard.java
+++ src/main/java/_2D_PlayerBoard.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 
 public class _2D_PlayerBoard {
 
-    public static void printBoard(int[][] board) {
+    public static void printBoard(int[][] board, int pos1, int pos2) {
+        int counter = 0;
+        board[pos1][pos2] = 1;
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                System.out.println(board[i][j]);
+                System.out.print(" " + board[i][j] + " ");
+                counter++;
+                if (counter == 5) {
+                    System.out.println("");
+                    counter = 0;
+                }
             }
         }
+        board[pos1][pos2] = 0;
     }
 
 
@@ -18,58 +27,56 @@ public class _2D_PlayerBoard {
         } else if (pos2 != 0) {
             pos2--;
             return pos2;
-        } else {
-            return pos2;
+        } else
+        return pos2;
         }
-
-    }
 
     public static int moveUpDown(int pos1, int minus) {
         if (minus == 0 && pos1 != 4) {
             pos1++;
             return pos1;
         } else if (pos1 != 0) {
-            pos1++;
+            pos1--;
             return pos1;
-        } else {
+        } else
             return pos1;
         }
-    }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] board = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+        int[][] board = new int[5][5];
         int pos1 = 0;
         int pos2 = 0;
-        int minus = 0;
 
         System.out.println("Press 1 to start");
         int start = sc.nextInt();
 
         if (start == 1) {
             while (true) {
+                printBoard(board,pos1,pos2);
+                int minus = 0;
                 System.out.println("Press W to move up, Press S to move down, Press D to move right, Press A to move left");
                 char input = sc.next().charAt(0);
+                input = Character.toUpperCase(input);
                 switch (input) {
-                    case 'W':
+                    case 'S':
                         pos1 = moveUpDown(pos1, minus);
                         break;
-                    case 'S':
+                    case 'W':
                         minus++;
                         pos1 = moveUpDown(pos1, minus);
                         break;
-                    case 'D':
+                    case 'A':
                         minus++;
                         pos2 = moveLeftRight(pos2, minus);
                         break;
-                    case 'A':
+                    case 'D':
                         pos2 = moveLeftRight(pos2, minus);
                         break;
                     default:
                         System.out.println("invalid input");
                 }
-                printBoard(board);
             }
         } else {
             System.out.println("Invalid input");
